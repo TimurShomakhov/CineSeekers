d4b6d7bd1d90d4a3b2321178d528b9528f5a5fb1
Alexander  von der Bey <alexandervonderbey@gmail.com>
2025-01-22 17:31:26 +0100

    added scroll function to pop movies

--- javascript.js
+++ javascript.js
@@ -57,26 +57,30 @@ const size = "w780";
 // Popular movie container
 const popMovs = document.querySelector('#popular-movie-container');
 
+// Navigation Buttons
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
 // Function to append popular movies
 const displayPopMovs = (movie, container) => {
     
     // movie card (container for single movie)
     const movCard = document.createElement("div");
-    movCard.className = "rounded-md flex flex-col ";
+    movCard.className = "movie-card rounded-md flex-shrink-0 flex flex-col items-center";
     
     // full image URL
     const poster = `${IMAGE_BASE_URL}${size}${movie.poster_path}`;
     const movImg = document.createElement("img");
     movImg.src = poster;
     movImg.alt = movie.title
-    movImg.className = "object-fit"
+    movImg.className = "object-fit w-full rounded-md"
 
     const movTitle = document.createElement("h3");
-    movTitle.className = "text-lg font-semibold text-gray-900 mb-2"
+    movTitle.className = "font-semibold text-gray-900 mb-1"
     movTitle.textContent = movie.title;
 
     const releaseDate = document.createElement("p");
-    releaseDate.className = "font-light text-xs italic"
+    releaseDate.className = "font-light text-xs italic pb-4"
     releaseDate.textContent = movie.release_date;
 
     // Append elements to movie card
@@ -100,6 +104,20 @@ const fetchPopMovs = async () => {
         return [];
     };
 };
+
+// Function to handle navigation
+const slideContainer = (container, direction) => {
+  const containerWidth = container.offsetWidth;
+  if (direction === "next") {
+      container.scrollLeft += containerWidth;
+  } else {
+      container.scrollLeft -= containerWidth;
+  }
+};
+
+// Event Listeners for Buttons
+prevButton.addEventListener("click", () => slideContainer(popMovs, "prev"));
+nextButton.addEventListener("click", () => slideContainer(popMovs, "next"));
 
 // Call function on page load
 document.addEventListener("DOMContentLoaded", async () => {
